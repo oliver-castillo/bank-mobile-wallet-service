6e014f5c9c4e695f499e6aff4503ac81707cba89
Oliver Castillo <ocastilt@nttdata.com>
2024-12-05 15:11:50 -0500

    chore: initial commit

---
+++ src/main/java/com/bank/mobilewalletservice/BankMobileWalletServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bank.mobilewalletservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankMobileWalletServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankMobileWalletServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/mobilewalletservice/BankMobileWalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.mobilewalletservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankMobileWalletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
